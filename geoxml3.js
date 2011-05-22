6704ff811d78291b435a14a023d76aae9fb1546d
geocodezip@gmail.com <geocodezip@gmail.com@69273422-4b70-11de-b305-25f493754997>
2011-05-22 04:58:32 +0000

    fix Issue 29:Force xhrFetcher MIME type to 'text/html'

--- geoxml3.js
+++ geoxml3.js
@@ -1025,8 +1025,11 @@ geoXML3.fetchXML = function (url, callback) {
     geoXML3.log('Unable to create XHR object');
     callback(null);
   } else {
-    xhrFetcher.fetcher.open('GET', url, true);
-    xhrFetcher.fetcher.onreadystatechange = function () {
+      if (xhrFetcher.fetcher.overrideMimeType) {
+        xhrFetcher.fetcher.overrideMimeType('text/xml');
+      }
+      xhrFetcher.fetcher.open('GET', url, true);
+      xhrFetcher.fetcher.onreadystatechange = function () {
       if (xhrFetcher.fetcher.readyState === 4) {
         // Retrieval complete
         if (!!xhrFetcher.xhrtimeout)
